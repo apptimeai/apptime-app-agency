20a50ffe303c174ff56a1d04369a7d2553b82cc6
iAnd1710 <andrelima940@gmail.com>
2026-02-19 16:51:12 -0300

    refactor: adjust Google Tag Manager script loading in Nuxt config and update Bun lockfile.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -78,23 +78,20 @@ export default defineNuxtConfig({
           async: true,
         },
         {
-          src: 'https://www.googletagmanager.com/gtag/js?id=G-XNJN8N72CW',
-          async: true,
-        },
-        {
-          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5WDRCW2C');`,
+          innerHTML: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5WDRCW2C');
+          `,
           type: 'text/javascript',
         },
       ],
       noscript: [
         {
-          children:
+          innerHTML:
             '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WDRCW2C" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
-          tagPosition: 'bodyOpen',
         },
       ],
     },
