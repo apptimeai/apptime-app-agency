92663b5a47476e5de07f1dea679eb467a53bc6cf
iAnd1710 <andrelima940@gmail.com>
2026-02-19 16:38:28 -0300

    feat: implement Google Tag Manager for analytics tracking and update bun lockfile config version.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -82,13 +82,19 @@ export default defineNuxtConfig({
           async: true,
         },
         {
-          innerHTML: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag() { dataLayer.push(arguments); }
-            gtag('js', new Date());
-            gtag('config', 'G-XNJN8N72CW');
-          `,
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5WDRCW2C');`,
           type: 'text/javascript',
+        },
+      ],
+      noscript: [
+        {
+          children:
+            '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WDRCW2C" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+          tagPosition: 'bodyOpen',
         },
       ],
     },
